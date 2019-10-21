d5c32daccdbde5c1b9dd4bb3b7c1db8b9e07c16f
Josh Holtz <josh@rokkincat.com>
2019-10-21 07:14:34 -0500

    Initial Commit

---
+++ CrunchyGIF/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CrunchyGIF
+//
+//  Created by Josh Holtz on 10/21/19.
+//  Copyright © 2019 Josh Holtz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CrunchyGIF/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CrunchyGIF
+//
+//  Created by Josh Holtz on 10/21/19.
+//  Copyright © 2019 Josh Holtz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
